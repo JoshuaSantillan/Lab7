906cceeba1ccc1c56be7a3a6299d2395bd1bc0be
Josh <jsantill@ucsd.edu>
2021-05-16 22:21:49 -0700

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,11 @@
 
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
+const settingsPage = document.querySelector('header img');
+const homePage = document.querySelector('header h1');
+let entryEle = 0;
 
 // Make sure you register your service worker here too
-
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
@@ -12,7 +14,30 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+        newPost.id = ++entryEle;
+        newPost.addEventListener('click', () =>{
+            // call function in router.js to change current state of a single Entry
+            setState({page:'entry', id: newPost.id});
+        });
         document.querySelector('main').appendChild(newPost);
       });
     });
 });
+
+homePage.addEventListener('click', () => {
+  setState({page: 'homePage'});
+});
+
+settingsPage.addEventListener('click', () => {
+  setState({page: 'settingsPage'});
+});
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register("./sw.js").then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
--- sw.js
+++ sw.js
@@ -1,6 +1,58 @@
 // sw.js - Service Worker
-
-// You will need 3 event listeners:
+// You will need 3 e listeners:
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+const CACHE_NAME = 'allowedCache';
+const urlsToCache = 'https://cse110lab6.herokuapp.com/entries';
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Cache is opened');
+          return cache.add("https://cse110lab6.herokuapp.com/entries");
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(clients.claim());
+    const cacheAllowlist = ['allowedCache'];
+    e.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheAllowlist.indexOf(cacheName) === -1) 
+              return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+      caches.match(e.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) 
+            return response;
+
+          return fetch(e.request).then(
+            function(response) {
+              // Check for a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') 
+                return response;
+              
+              var responseToCache = response.clone();
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(e.request, responseToCache);
+                });
+              return response;
+            }
+          );
+        })
+      );
+});
